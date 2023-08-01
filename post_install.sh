a76ec2cb2ba1acd3c2cbd2e3738a305bbfcad36d
tailgear <a@kovyrev.ru>
2023-08-01 21:57:09 +0300

    first

---
+++ post_install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+mkdir /data
+chmod 755 /data
+
+# Enable the service
+sysrc -f /etc/rc.conf lidarr_enable="YES"
+sysrc -f /etc/rc.conf radarr_enable="YES"
+sysrc -f /etc/rc.conf readarr_enable="YES"
+sysrc -f /etc/rc.conf sonarr_enable="YES"
+sysrc -f /etc/rc.conf prowlarr_enable="YES"
+sysrc -f /etc/rc.conf transmission_enable="YES"
+sysrc -f /etc/rc.conf transmission_download_dir="/data"
+
+# syncthing generate --config=/usr/local/etc/syncthing --no-default-folder --gui-user=$GUI_USER --gui-password=$GUI_PASS
+
+# chown -R syncthing:syncthing /Sync
+
+# Start the service
+service lidarr start 2>/dev/null
+service radarr start 2>/dev/null
+service readarr start 2>/dev/null
+service sonarr start 2>/dev/null
+service prowlarr start 2>/dev/null
+service transmission start 2>/dev/null
+sleep 2 # It can take a few seconds.
+
+# Without the whitelist disabled, the user will not be able to access it. They can reenable and manually whitelist their IP's.
+SETTINGS="/usr/local/etc/transmission/home/settings.json"
+
+echo "Disabling RPC whitelist, you may want to reenable it with the specific IP's you will access transmission with by editing $SETTINGS"
+sed -i '' -e 's/\([[:space:]]*"rpc-whitelist-enabled":[[:space:]]*\)true,/\1false,/' $SETTINGS
+
+service transmission reload
